76860d1ba6256b8bf9d1d3ed4da58ae5b5d257f1
K S Sarika <sarika.ks.official@gmail.com>
2025-03-15 12:07:06 +0530

    Msg dispplayed on no student attented

--- client/esm-client/src/Teacher/TestStatus/TestStatus.js
+++ client/esm-client/src/Teacher/TestStatus/TestStatus.js
@@ -3,6 +3,7 @@ import { Row, Col } from "antd";
 import { connect } from "react-redux";
 import StudentStatus from "./StudentStatus";
 
+
 function TestStatus(props) {
   console.log("selected Test",props.selectedTest);
   const style = { background: "#0092ff", padding: "8px 0" };
@@ -11,18 +12,27 @@ function TestStatus(props) {
   const testName = props.selectedTest.testName;
   return (
     <>
+      <div>
+        <h1 className="container heading_status">{testName}</h1>
+      </div>
       <div className="container student__status">
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          {students.length > 0 &&
-            students.map((student, index) => (
-              <StudentStatus
-                className={className}
-                testName={testName}
-                student={student}
-                key={index}
-              />
-            ))}
-        </Row>
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        {students.length > 0 ? (
+          students.map((student, index) => (
+            <StudentStatus
+              className={className}
+              testName={testName}
+              student={student}
+              key={index}
+            />
+          ))
+        ) : (
+          <div style={{ color: 'red', fontWeight: 'bold'}}>
+            No student attempted this test.
+          </div>
+        )}
+      </Row>
+
       </div>
     </>
   );
